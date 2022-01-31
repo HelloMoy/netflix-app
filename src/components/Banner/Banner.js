99cd7534dfa133099299e859568e29f7252abda6
Moises Astudillo <bruma_moy@hotmail.com>
2022-01-30 22:50:25 -0600

    [x] Improve css styles of the Banner component. [ ] Add scroll with buttons to MovieCarousel component.

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,16 +4,19 @@ const Banner = () => {
     return (
         <div className={styles.banner}>
             <div className={styles.banner__container}>
-                <img
-                    className={styles.banner__image}
-                    src="https://image.tmdb.org/t/p/original/iQFcwSGbZXMkeyKrxbPnwnRo5fl.jpg"
-                    alt="Spider-Man: No Way Home"
-                />
-                <div className={styles.banner__boxShadow}>
-                    <div className={styles.banner__textContainer}>
-                        <h2 className={styles.banner__title}>Spider-Man: No Way Home</h2>
+                <div className={styles.banner__imageContainer}>
+                    <img
+                        className={styles.banner__image}
+                        src="https://image.tmdb.org/t/p/original/3G1Q5xF40HkUBJXxt2DQgQzKTp5.jpg"
+                        alt="Spider-Man: No Way Home"
+                    />
+                    <div className={styles.banner__boxShadow}></div>
+                </div>
+                <div className={styles.banner__textContainer}>
+                    <div className={styles.banner__text}>
+                        <h2 className={styles.banner__title}>Encanto</h2>
                         <p className={styles.banner__overview}>
-                        Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.
+                        The tale of an extraordinary family, the Madrigals, who live hidden in the mountains of Colombia, in a magical house, in a vibrant town, in a wondrous, charmed place called an Encanto. The magic of the Encanto has blessed every child in the family with a unique gift from super strength to the power to heal—every child except one, Mirabel. But when she discovers that the magic surrounding the Encanto is in danger, Mirabel decides that she, the only ordinary Madrigal, might just be her exceptional family's last hope.
                         </p>
                     </div>
                 </div>
