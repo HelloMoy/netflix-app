8e74d92bcc05f973c17bcd307eedeab84f04506f
Moises Astudillo <bruma_moy@hotmail.com>
2022-01-30 20:35:27 -0600

    [x] Add text to Banner component. [ ] Add scroll with buttons to MovieCarousel component.

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,4 +1,4 @@
-import styles from './Banner.module.css'
+import styles from './Banner.module.css';
 
 const Banner = () => {
     return (
@@ -6,13 +6,16 @@ const Banner = () => {
             <div className={styles.banner__container}>
                 <img
                     className={styles.banner__image}
-                    // src="https://site.groupe-psa.com/content/uploads/sites/3/2016/12/white-background-2.jpg"
                     src="https://image.tmdb.org/t/p/original/iQFcwSGbZXMkeyKrxbPnwnRo5fl.jpg"
                     alt="Spider-Man: No Way Home"
                 />
-
-                <div className={styles.banner__textContainer}>
-                    Text Banner
+                <div className={styles.banner__boxShadow}>
+                    <div className={styles.banner__textContainer}>
+                        <h2 className={styles.banner__title}>Spider-Man: No Way Home</h2>
+                        <p className={styles.banner__overview}>
+                        Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
