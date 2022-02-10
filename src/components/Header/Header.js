6b72892bc6822c44cd3c6c9dd465e48e9f1350b9
Moises Astudillo <bruma_moy@hotmail.com>
2022-02-09 21:17:25 -0600

    [x] Add search input and style it. [ ] Implement search by movie name.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Lens from '../../assets/icons/svgComponents/Lens';
 import MenuBar from '../../assets/icons/svgComponents/MenuBar';
@@ -6,11 +6,30 @@ import CategoriesSection from '../CategoriesSection/';
 import styles from './Header.module.css';
 
 const Header = () => {
+    const [showSearchInput, setShowSearchInput] = useState(false);
+    const searchInputRef = useRef();
 
     const [showCategoriesSection, setShowCategoriesSection] = useState(false);
 
     const toggleShowCategoriesSection = () => {
         setShowCategoriesSection((previousState) => !previousState);
+    }
+
+    const onClearSearchInput = () => {
+        searchInputRef.current.value = '';
+        searchInputRef.current.focus();
+    }
+
+    const onLensIconClick = () => {
+        setShowSearchInput(!showSearchInput);
+        searchInputRef.current.focus();
+    }
+
+    const handleOnSubmitSearchInput = (event) => {
+        event.preventDefault();
+        console.log(searchInputRef.current.value);
+        searchInputRef.current.value = '';
+        onLensIconClick();
     }
 
     return (
@@ -35,8 +54,20 @@ const Header = () => {
                 </Link>
             </div>
             <div className={styles.header__rightSection}>
-                <div className={styles.header__lensIconContainer}>
-                    <Lens className={styles.header__lensIcon} />
+                <div className={`${styles.search} ${(showSearchInput ? styles.search__extend : '')}`}>
+                    <div className={styles.searchIcon} onClick={onLensIconClick}>
+                        <div className={styles.searchIcon__container}>
+                            <Lens className={styles.searchIcon__container__svg} />
+                        </div>
+                    </div>
+                    <div className={styles.search__formContainer}>
+                        <form action="submit" className={styles.search__form} onSubmit={handleOnSubmitSearchInput}>
+                            <input type="text" ref={searchInputRef} className={styles.search__input} />
+                        </form>
+                    </div>
+                    <div className={styles.search__clearInput} onClick={onClearSearchInput}>
+                        <p className={styles.search__clearInputIcon}>x</p>
+                    </div>
                 </div>
             </div>
             <CategoriesSection
